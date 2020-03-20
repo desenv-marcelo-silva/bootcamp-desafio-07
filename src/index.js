e2f9589718f662b16fa40ca23a0d60fc329a4eef
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-03-20 10:33:18 -0300

    Retirada do app padrão react-native.

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function App() {
+  return (
+    <View style={{backgroundColor: '#7159c1'}}>
+      <Text
+        style={{
+          textAlign: 'center',
+          alignContent: 'center',
+          color: '#FFF',
+        }}>
+        App Rocketshoes - Challenge #7
+      </Text>
+    </View>
+  );
+}
+
+export default App;
