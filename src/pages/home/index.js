aabfdc0eeccbf4297bfe8f72437c2a5d11b8d9e3
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-03-22 14:21:56 -0300

    Configuração/estilização da home.

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,65 @@
 import React from 'react';
 import { Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import {
+  Container,
+  List,
+  Product,
+  ProductList,
+  Imagem,
+  ButtonArea,
+  ButtonAddCart,
+  ButtonAddCartText,
+  Titulo,
+  Preco,
+} from './styles';
+
+const DATA = [
+  {
+    id: 1,
+    title: 'Tênis de Caminhada Leve Confortável',
+    price: 179.9,
+    image:
+      'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+  },
+  {
+    id: 2,
+    title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
+    price: 139.9,
+    image:
+      'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
+  },
+  {
+    id: 3,
+    title: 'Tênis Adidas Duramo Lite 2.0',
+    price: 219.9,
+    image:
+      'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis3.jpg',
+  },
+];
 
 export default function home({ navigation }) {
   return (
     <Container>
+      <List>
+        <ProductList>
+          {DATA.map(product => (
+            <Product key={product.id}>
+              <Imagem source={{ uri: product.image }} />
+              <Titulo>{product.title}</Titulo>
+              <Preco>{product.price}</Preco>
+              <ButtonArea>
+                <Icon name="add-shopping-cart" size={20} color="#FFF" />
+                <Text>3</Text>
+                <ButtonAddCart onPress={() => {}}>
+                  <ButtonAddCartText>Adicionar ao carrinho</ButtonAddCartText>
+                </ButtonAddCart>
+              </ButtonArea>
+            </Product>
+          ))}
+        </ProductList>
+      </List>
       <Text onPress={() => navigation.navigate('Cart')}>Home</Text>
     </Container>
   );
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,6 +1,68 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
-  padding: 20px;
+  margin-top: 30px;
+`;
+
+export const List = styled.View`
+  flex: 1;
+  background: #000;
+  align-items: center;
+`;
+
+export const ProductList = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+})`
+  margin-top: 20px;
+`;
+
+export const Product = styled.View`
+  flex: 1;
+  background: #fff;
+  margin: 10px;
+  padding: 10px 20px;
+  max-width: 300px;
+  border-radius: 5px;
+`;
+
+export const Titulo = styled.Text.attrs({
+  numberOfLines: 2,
+})`
+  font-size: 16px;
+  margin-bottom: auto;
+`;
+
+export const Preco = styled.Text`
+  font-size: 20px;
+  margin-left: 10px;
+  font-weight: 700;
+`;
+
+export const Imagem = styled.Image`
+  width: 250px;
+  height: 250px;
+  align-self: center;
+`;
+
+export const ButtonArea = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-content: space-between;
+  align-items: center;
+  background: #7159c1;
+  border-radius: 4px;
+  padding: 10px;
+  margin-top: auto;
+`;
+
+export const ButtonAddCart = styled(RectButton)``;
+
+export const ButtonAddCartText = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #fff;
+  text-transform: uppercase;
 `;
