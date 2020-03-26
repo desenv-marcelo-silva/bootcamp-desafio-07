8652589aea3bfc02a74e96c5d71bacfd1223f41d
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-03-26 00:06:39 -0300

    Transformar Home em stateComponent

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
 
 import {
   Container,
@@ -18,58 +20,46 @@ import {
   Price,
 } from './styles';
 
-const DATA = [
-  {
-    id: 1,
-    title: 'Tênis de Caminhada Leve Confortável',
-    price: 179.9,
-    image:
-      'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
-  },
-  {
-    id: 2,
-    title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
-    price: 139.9,
-    image:
-      'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
-  },
-  {
-    id: 3,
-    title: 'Tênis Adidas Duramo Lite 2.0',
-    price: 219.9,
-    image:
-      'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis3.jpg',
-  },
-];
+export default class Home extends Component {
+  state = { products: [] };
 
-export default function home({ navigation }) {
-  return (
-    <Container>
-      <List>
-        <ProductList>
-          {DATA.map(product => (
-            <Product key={product.id}>
-              <AreaPhoto>
-                <Image source={{ uri: product.image }} />
-              </AreaPhoto>
-              <AreaPriceTitle>
-                <Title>{product.title}</Title>
-                <Price>{product.price}</Price>
-              </AreaPriceTitle>
-              <AreaButton>
-                <IconArea>
-                  <Icon name="add-shopping-cart" size={20} color="#FFF" />
-                  <Quantity>3</Quantity>
-                </IconArea>
-                <ButtonAddCart onPress={() => navigation.navigate('Cart')}>
-                  <ButtonAddCartText>Adicionar ao carrinho</ButtonAddCartText>
-                </ButtonAddCart>
-              </AreaButton>
-            </Product>
-          ))}
-        </ProductList>
-      </List>
-      {/* <Text onPress={() => navigation.navigate('Cart')}>Home</Text> */}
-    </Container>
-  );
+  async componentDidMount() {
+    const response = await api.get('products');
+    console.tron.warn(response.data);
+    this.setState({ products: response.data });
+  }
+
+  render() {
+    const { products } = this.state;
+    const { navigation } = this.props;
+
+    return (
+      <Container>
+        <List>
+          <ProductList>
+            {products.map(product => (
+              <Product key={product.id}>
+                <AreaPhoto>
+                  <Image source={{ uri: product.image }} />
+                </AreaPhoto>
+                <AreaPriceTitle>
+                  <Title>{product.title}</Title>
+                  <Price>{product.price}</Price>
+                </AreaPriceTitle>
+                <AreaButton>
+                  <IconArea>
+                    <Icon name="add-shopping-cart" size={20} color="#FFF" />
+                    <Quantity>3</Quantity>
+                  </IconArea>
+                  <ButtonAddCart onPress={() => navigation.navigate('Cart')}>
+                    <ButtonAddCartText>Adicionar ao carrinho</ButtonAddCartText>
+                  </ButtonAddCart>
+                </AreaButton>
+              </Product>
+            ))}
+          </ProductList>
+        </List>
+      </Container>
+    );
+  }
 }
