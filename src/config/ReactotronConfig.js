e5efb5a45e4e93bae4504585f1b87bce918f7e87
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-03-28 16:31:15 -0300

    Reactotron para redux-saga: instalação e configuração.

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,10 +1,13 @@
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 
+import reactotronSaga from 'reactotron-redux-saga';
+// host passado para o reactotron por conta do liveReload via USB
 if (process.env.NODE_ENV === 'development') {
   const tron = Reactotron.configure({ host: '172.30.172.10' })
     .useReactNative()
     .use(reactotronRedux())
+    .use(reactotronSaga())
     .connect();
 
   console.tron = tron;
