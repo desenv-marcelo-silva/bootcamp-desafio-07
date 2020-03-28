55a7ce0ff78b3ba4c6ce130fb88c6dfacdc21859
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-03-28 12:27:25 -0300

    Live reload com Reactotron - para USB device.

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -2,7 +2,7 @@ import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 
 if (process.env.NODE_ENV === 'development') {
-  const tron = Reactotron.configure()
+  const tron = Reactotron.configure({ host: '172.30.172.10' })
     .useReactNative()
     .use(reactotronRedux())
     .connect();
